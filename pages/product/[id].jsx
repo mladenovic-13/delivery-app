2aedd077222015772d8b293360c6b493d8791e33
mladenovic13 <nikolanik999@gmail.com>
2022-05-20 19:19:22 +0200

    Product Page Done

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -28,11 +28,14 @@ const ImgContainer = styled.div`
 const PizzaName = tw.h1`text-3xl font-semibold`;
 const PizzaPrice = tw.div`text-red-700 text-2xl font-medium my-4`;
 const PizzaDesc = tw.div`text-xl text-gray-700 font-light`;
-const SizeLabel = tw.h3`text-lg font-semibold my-4`;
+const SizeLabel = tw.h3`text-xl font-semibold my-4`;
 const Sizes = styled.div`
   width: 50%;
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  padding-top: 5px;
+  padding-bottom: 10px;
 `;
 const Size = styled.div`
   width: 50px;
@@ -60,6 +63,55 @@ const SizeNum = styled.div`
   font-weight: 700;
 `;
 
+const PizzaExtras = styled.div`
+  h3 {
+    font-size: 20px;
+    font-weight: 600;
+  }
+  .container {
+    margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 120px;
+  }
+  .option {
+    margin-left: 10px;
+    display: flex;
+    align-items: center;
+    font-size: 17px;
+    font-weight: 400;
+  }
+  .checkbox {
+    margin-right: 10px;
+    width: 16px;
+    height: 16px;
+    background-color: red;
+  }
+`;
+
+const AddToCart = styled.div`
+  .quantity {
+    width: 50px;
+    height: 40px;
+    text-align: center;
+    border: 2px solid red;
+    &:focus {
+      outline: red;
+    }
+  }
+  .add {
+    height: 40px;
+    padding: 1px 5px;
+    background-color: red;
+    color: white;
+    font-weight: 600;
+    border: none;
+    margin-left: 10px;
+  }
+  margin-top: 15px;
+`;
+
 const Product = () => {
   const [size, setSize] = useState(0);
   const pizza = {
@@ -83,19 +135,70 @@ const Product = () => {
         <PizzaDesc>{pizza.desc}</PizzaDesc>
         <SizeLabel>Choose youre size:</SizeLabel>
         <Sizes>
-          <Size>
+          <Size onClick={() => setSize(0)}>
             <Image src="/img/size.png" alt="" layout="fill" />
             <SizeNum>Small</SizeNum>
           </Size>
-          <Size>
+          <Size onClick={() => setSize(1)}>
             <Image src="/img/size.png" alt="" layout="fill" />
             <SizeNum>Medium</SizeNum>
           </Size>
-          <Size>
+          <Size onClick={() => setSize(2)}>
             <Image src="/img/size.png" alt="" layout="fill" />
             <SizeNum>Large</SizeNum>
           </Size>
         </Sizes>
+        <PizzaExtras>
+          <h3>Choose extras:</h3>
+          <div className="container">
+            <div className="option">
+              <input
+                type="checkbox"
+                name="double"
+                id="double"
+                className="checkbox"
+              />
+              <label htmlFor="double">Double Ingredients</label>
+            </div>
+            <div className="option">
+              <input
+                type="checkbox"
+                name="extracheese"
+                id="extracheese"
+                className="checkbox"
+              />
+              <label htmlFor="extracheese">Extra Cheese</label>
+            </div>
+            <div className="option">
+              <input
+                type="checkbox"
+                name="extraham"
+                id="extraham"
+                className="checkbox"
+              />
+              <label htmlFor="extraham">Extra Ham</label>
+            </div>
+            <div className="option">
+              <input
+                type="checkbox"
+                name="extraolives"
+                id="extraolives"
+                className="checkbox"
+              />
+              <label htmlFor="extraolives">Extra Olives</label>
+            </div>
+          </div>
+        </PizzaExtras>
+        <AddToCart>
+          <input
+            type="number"
+            name="quantity"
+            id="quantity"
+            defaultValue={1}
+            className="quantity"
+          />
+          <button className="add">ADD TO CART</button>
+        </AddToCart>
       </Right>
     </ProductContainer>
   );
